10f1e66407a854ded76799fd8bc9d4b2317c7393
tulabhagya2 <tulabhagyasri1@gmail.com>
2025-11-14 11:34:19 +0530

    this is my first commit

---
+++ Module-2/Day-5/q1/prime.js
@@ -0,0 +1,16 @@
+let num=5;
+function prime(num){
+    let count=0;
+    for(let i=1;i<num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(prime(num));
