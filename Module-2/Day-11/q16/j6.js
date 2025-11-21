9e7d44af96b0675f751d3d4c079c1c37ab5bd66c
tulabhagya2 <tulabhagyasri1@gmail.com>
2025-11-21 20:49:09 +0530

    Arrowsfunctions and ternary operator

---
+++ Module-2/Day-11/q16/j6.js
@@ -0,0 +1,15 @@
+
+//even
+
+const even=(n)=>{
+    if(n%2==0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(even(4));
+//ternary opertor
+let res=(marks>=15)?"pass":"Fail";
+console.log(res)
