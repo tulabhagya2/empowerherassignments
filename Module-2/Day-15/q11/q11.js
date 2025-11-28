d397e9ea382d844890803ed96ed96adeeeae3325
tulabhagya2 <tulabhagyasri1@gmail.com>
2025-11-28 10:38:51 +0530

    promises commit

---
+++ Module-2/Day-15/q11/q11.js
@@ -0,0 +1,13 @@
+function displayMessage(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+function getUserInput(callback) {
+  setTimeout(() => {
+    let username = "Alice"; 
+    callback(username);      
+  }, 1000);
+}
+
+
+getUserInput(displayMessage);
