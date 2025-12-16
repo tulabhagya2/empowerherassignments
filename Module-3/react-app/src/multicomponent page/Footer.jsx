784f3342cd5115943fcbea92d7b0322be9b8fc09
tulabhagya2 <tulabhagyasri1@gmail.com>
2025-12-16 20:10:26 +0530

    this is multicomponent commit

---
+++ Module-3/react-app/src/multicomponent page/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ textAlign: "center", fontSize: "14px", color: "gray", marginTop: "20px" }}>
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Module-3/react-app/src/multicomponent page/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div style={{ backgroundColor: "#4CAF50", padding: "10px", color: "white" }}>
+      This is Navbar
+    </div>
+  );
+}
+
+export default Navbar;
